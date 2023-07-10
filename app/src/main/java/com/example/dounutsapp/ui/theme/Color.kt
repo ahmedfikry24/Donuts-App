eca5d1d991caf150cbc460350c137939384dbbd6
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-07-10 22:43:04 +0300

    update colors

--- app/src/main/java/com/example/dounutsapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/dounutsapp/ui/theme/Color.kt
@@ -9,3 +9,18 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val black = Color(0XFF000000)
+val black60  = Color(0x99000000)
+val black80  = Color(0xCC000000)
+
+val white  = Color(0XFFFFFFFF)
+val whiteBlue  = Color(0XFFD7E4F6)
+val whitePink = Color(0XFFFFC7D0)
+
+val lightBlue  = Color(0x66130F26)
+
+val lightPink = Color(0XFFFED8DF)
+val pink = Color(0XFFFF9494)
+val DarkPink = Color(0XFFFF7074)
+val deepDarkPink = Color(0XFFFE554A)
