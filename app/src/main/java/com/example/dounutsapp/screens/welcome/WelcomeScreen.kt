e97a006bf29b3a541d994da6e011398fee7f06c3
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-07-10 22:42:36 +0300

    add WelcomeScreen

---
+++ app/src/main/java/com/example/dounutsapp/screens/welcome/WelcomeScreen.kt
@@ -0,0 +1,14 @@
+package com.example.dounutsapp.screens.welcome
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WelcomeScreen(padding: PaddingValues) {
+    WelcomeContent(padding)
+}
+
+@Composable
+private fun WelcomeContent(padding: PaddingValues) {
+
+}
