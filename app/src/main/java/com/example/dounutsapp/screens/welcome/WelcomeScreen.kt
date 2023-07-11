474cbab786a5b503f19fe1bcc8c3aecca24df950
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-07-11 04:15:04 +0300

    update WelcomeScreen

--- app/src/main/java/com/example/dounutsapp/screens/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/example/dounutsapp/screens/welcome/WelcomeScreen.kt
@@ -1,14 +1,91 @@
 package com.example.dounutsapp.screens.welcome
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.dounutsapp.R
+import com.example.dounutsapp.screens.VerticalSpacer
+import com.example.dounutsapp.screens.welcome.composable.DonutsImages
+import com.example.dounutsapp.ui.theme.DarkPink
+import com.example.dounutsapp.ui.theme.black
+import com.example.dounutsapp.ui.theme.buttonText
+import com.example.dounutsapp.ui.theme.lightPink
+import com.example.dounutsapp.ui.theme.pink
+import com.example.dounutsapp.ui.theme.size50
+import com.example.dounutsapp.ui.theme.size67
+import com.example.dounutsapp.ui.theme.space16
+import com.example.dounutsapp.ui.theme.space40
+import com.example.dounutsapp.ui.theme.welcomeDescription
+import com.example.dounutsapp.ui.theme.welcomeText
+import com.example.dounutsapp.ui.theme.white
 
 @Composable
 fun WelcomeScreen(padding: PaddingValues) {
-    WelcomeContent(padding)
+    WelcomeContent(padding, onClickButton = {})
 }
 
 @Composable
-private fun WelcomeContent(padding: PaddingValues) {
-
+private fun WelcomeContent(padding: PaddingValues, onClickButton: () -> Unit) {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .background(lightPink)
+            .padding(padding)
+    ) {
+        DonutsImages()
+        Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+            Text(
+                modifier = Modifier.padding(start = space40),
+                text = "Gonuts\n" +
+                        "with\n" +
+                        "Donuts",
+                style = welcomeText,
+                color = DarkPink,
+            )
+            Image(
+                modifier = Modifier.size(80.dp, 150.dp),
+                painter = painterResource(R.drawable.image_4), contentDescription = "",
+                contentScale = ContentScale.FillBounds
+            )
+        }
+        VerticalSpacer(height = space16)
+        Text(
+            modifier = Modifier.padding(horizontal = space40),
+            text = stringResource(R.string.description_welcome),
+            style = welcomeDescription,
+            color = pink
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        TextButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(size67)
+                .padding(horizontal = space40),
+            colors = ButtonDefaults.buttonColors(white),
+            shape = RoundedCornerShape(size50),
+            onClick = onClickButton
+        ) {
+            Text(text = stringResource(R.string.get_started), style = buttonText, color = black)
+        }
+        Spacer(modifier = Modifier.weight(1f))
+    }
 }
