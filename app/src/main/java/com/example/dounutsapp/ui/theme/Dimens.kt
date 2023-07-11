8d6ae1f68ff1121316c594d2300c16fb35071431
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-07-11 04:14:10 +0300

    add dimens

---
+++ app/src/main/java/com/example/dounutsapp/ui/theme/Dimens.kt
@@ -0,0 +1,19 @@
+package com.example.dounutsapp.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+val size180 = 160.dp
+val size120 = 100.dp
+val size340 = 310.dp
+val size100 = 80.dp
+val size67 = 67.dp
+val size50 = 50.dp
+
+val space40 = 40.dp
+val space16 = 16.dp
+val space20 = 20.dp
+
+val text54 =54.sp
+val text20 =20.sp
+val text18 =18.sp
