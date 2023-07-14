ae93b0bb10f80d518b60d678d29e24f864c3ac87
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-07-14 04:59:16 +0300

    update MainActivity

--- app/src/main/java/com/example/dounutsapp/DonutsApp.kt
+++ app/src/main/java/com/example/dounutsapp/DonutsApp.kt
@@ -3,12 +3,13 @@ package com.example.dounutsapp
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import com.example.dounutsapp.screens.home.HomeScreen
+import androidx.navigation.compose.rememberNavController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DonutsApp() {
     Scaffold { padding ->
-        HomeScreen(padding)
+        val navController = rememberNavController()
+        DonutAppGraph(padding = padding, navController = navController)
     }
 }
