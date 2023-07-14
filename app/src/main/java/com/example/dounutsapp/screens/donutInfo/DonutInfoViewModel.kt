f543a14feca938a32c7f2c2bc5f7b091387f933c
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-07-14 04:28:37 +0300

    add DonutInfoViewModel

---
+++ app/src/main/java/com/example/dounutsapp/screens/donutInfo/DonutInfoViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.dounutsapp.screens.donutInfo
+
+import androidx.lifecycle.ViewModel
+import com.example.dounutsapp.screens.donutInfo.state.DonutInfoUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+@HiltViewModel
+class DonutInfoViewModel : ViewModel() {
+
+    private val _state = MutableStateFlow(DonutInfoUiState())
+    val state = _state.asStateFlow()
+
+    fun onClickMines() {
+        if (state.value.quantity > 0) {
+            _state.update {
+                it.copy(
+                    quantity = state.value.quantity - 1,
+                    quantityStatus = false,
+                    totalPrice = state.value.totalPrice - 16
+                )
+            }
+        }
+    }
+
+    fun onClickPlus() {
+        _state.update {
+            it.copy(
+                quantity = state.value.quantity + 1,
+                quantityStatus = true,
+                totalPrice = state.value.totalPrice + 16
+            )
+        }
+    }
+}
